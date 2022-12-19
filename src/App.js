3e06a5d7a1a3705873ab53a0b159f50e68a53295
Luis Muñoz <l.munoz.m@outlook.com>
2022-12-19 14:00:17 -0300

    Create navbar and setup folder and file base.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Navbar from './navbar/Navbar';
+import Rockets from './rockets/Rockets';
+import Missions from './missions/Missions';
+import MyProfile from './myprofile/MyProfile';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import planet from '../images/planet.png';
+
+const Navbar = () => (
+  <div className="nav-bar">
+    <a className="nav-brand" href="#/"><img className="navbar-logo" alt="" src={planet} /></a>
+    <ul className="nav-list">
+      <li><NavLink to="/" className={(navData) => (navData.isActive ? 'nav-link active-link' : 'nav-link')}>Rockets</NavLink></li>
+      <li><NavLink to="/missions" className={(navData) => (navData.isActive ? 'nav-link active-link' : 'nav-link')}>Missions</NavLink></li>
+      <li><NavLink to="/myprofile" className={(navData) => (navData.isActive ? 'nav-link active-link' : 'nav-link')}>My Profile</NavLink></li>
+    </ul>
+  </div>
+);
+
+export default Navbar;
