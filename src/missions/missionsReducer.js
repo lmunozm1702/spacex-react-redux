1c7607b17cb74c8a402bd3b141c9f5d3614372a6
amexabee <amexabee@yahoo.com>
2022-12-22 01:07:25 +0200

    Implement mission joining

--- src/missions/missionsReducer.js
+++ src/missions/missionsReducer.js
@@ -1,25 +1,53 @@
-import { GET_MISSIONS, GET_MISSIONS_FULFILLED } from './actions';
+import {
+  GET_MISSIONS,
+  GET_MISSIONS_FULFILLED,
+  STATUS_UPDATED,
+} from './actions';
 
 export const fetchMissions = (payload) => ({
   type: GET_MISSIONS,
   payload,
 });
 
+export const changeStatus = (payload) => ({
+  type: STATUS_UPDATED,
+  payload,
+});
+
 const missionsReducer = (state = [], action = {}) => {
-  let result = [];
+  let newState = [];
   switch (action.type) {
     case GET_MISSIONS_FULFILLED:
       action.payload.forEach((element) => {
-        result = [
-          ...result,
+        newState = [
+          ...newState,
           {
             id: element.mission_id,
             name: element.mission_name,
             description: element.description,
+            status: 'NOT A MEMBER',
+            join: 'Join Missions',
           },
         ];
       });
-      return result;
+      return newState;
+    case STATUS_UPDATED: {
+      newState = [
+        ...state.filter((mission) => mission.id !== action.payload.id),
+        {
+          ...action.payload,
+          status:
+            action.payload.status === 'NOT A MEMBER'
+              ? 'ACTIVE MEMBER'
+              : 'NOT A MEMBER',
+          join:
+            action.payload.status === 'NOT A MEMBER'
+              ? 'Leave Missions'
+              : 'Join Missions',
+        },
+      ];
+      return newState;
+    }
     default:
       return state;
   }
