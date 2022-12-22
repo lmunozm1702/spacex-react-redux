b0f10d7515d43c3bc434eb1420abf516c6a2c1c8
Luis Muñoz <l.munoz.m@outlook.com>
2022-12-21 22:08:13 -0300

    Fix issues

--- src/ConfigStore.js
+++ src/ConfigStore.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rockets from './rockets/rocketsReducer';
-import Missions from './missions/Missions';
 import missions from './missions/missionsReducer';
 import MyProfile from './myprofile/MyProfile';
 
