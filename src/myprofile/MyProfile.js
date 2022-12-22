8b2c90770291fd61a896392fc2e90efa334001f1
Luis Muñoz <l.munoz.m@outlook.com>
2022-12-22 07:27:20 -0300

    Add render reserved rockets list.
    Create MyRockets.js to render the reserved rocket list.
    Update MyProfile.js to display the reserved rockets.
    Update rocketsReducer.js to remove comments.

--- src/myprofile/MyProfile.js
+++ src/myprofile/MyProfile.js
@@ -1,3 +1,10 @@
+import MyRockets from '../rockets/MyRockets';
+
 export default function MyProfile() {
-  return (<div>MyProfile!</div>);
+  return (
+    <div>
+      <h3>My Rockets</h3>
+      <MyRockets />
+    </div>
+  );
 }
