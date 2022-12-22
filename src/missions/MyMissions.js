fd42565e5374fa488df6891628d6fb5a3448678d
amexabee <amexabee@yahoo.com>
2022-12-22 23:54:06 +0200

    Do more styling

--- src/missions/MyMissions.js
+++ src/missions/MyMissions.js
@@ -2,15 +2,11 @@ import { useSelector } from 'react-redux';
 
 const MyMissions = () => {
   const missionList = useSelector((state) => state.missions);
-  return (
-    <ul className="list-group w-75">
-      {missionList.map((mission) => (mission.reserved ? (
-        <li className="list-group-item" key={mission.id}>
-          {mission.name}
-        </li>
-      ) : null))}
-    </ul>
-  );
+  return missionList.map((mission) => (mission.reserved ? (
+    <div className="my-item" key={mission.id}>
+      {mission.name}
+    </div>
+  ) : null));
 };
 
 export default MyMissions;
