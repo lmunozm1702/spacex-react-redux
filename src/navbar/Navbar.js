cb658ddd748b888e4d27093d78cae20448ad3424
Luis Muñoz <l.munoz.m@outlook.com>
2022-12-22 15:40:59 -0300

    Solve github conflict on MyProfile.js

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -7,7 +7,7 @@ import planet from '../images/planet.png';
 const Navbar = () => (
   <div className="nav-bar">
     <a className="nav-brand" href="#/"><img className="navbar-logo" alt="" src={planet} /></a>
-    <ul className="nav-list" id="list-nav">
+    <ul className="nav-list">
       <li><NavLink to="/" className={(navData) => (navData.isActive ? 'nav-link active-link' : 'nav-link')}>Rockets</NavLink></li>
       <li><NavLink to="/missions" className={(navData) => (navData.isActive ? 'nav-link active-link' : 'nav-link')}>Missions</NavLink></li>
       <li><NavLink to="/myprofile" className={(navData) => (navData.isActive ? 'nav-link active-link' : 'nav-link')}>My Profile</NavLink></li>
