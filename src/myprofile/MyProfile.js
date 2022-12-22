cd7704651d0e85558ec3836e92d5da62884878c6
amexabee <amexabee@yahoo.com>
2022-12-22 16:26:47 +0200

    Add mission in myprofile

--- src/myprofile/MyProfile.js
+++ src/myprofile/MyProfile.js
@@ -1,10 +1,17 @@
+import MyMissions from '../missions/MyMissions';
 import MyRockets from '../rockets/MyRockets';
 
 export default function MyProfile() {
   return (
-    <div>
-      <h3>My Rockets</h3>
-      <MyRockets />
+    <div className="container d-flex justify-content-around mt-5">
+      <div className="w-50">
+        <h3>My Missions</h3>
+        <MyMissions />
+      </div>
+      <div className="w-50">
+        <h3>My Rockets</h3>
+        <MyRockets />
+      </div>
     </div>
   );
 }
